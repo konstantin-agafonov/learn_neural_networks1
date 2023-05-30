85bab02c64b54681259894c7a504f7e169dafb48
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2023-05-30 20:48:54 +0300

    Created ReLU and data

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from LayerDense import LayerDense
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+import matplotlib.pyplot as plt
 
-X = [
+from LayerDense import LayerDense
+from ActivationReLU import ActivationReLU
+
+nnfs.init()
+np.random.seed(0)
+
+'''X = [
     [1, 2, 3, 2.5],
     [2, 5, -1, 2],
     [-1.5, 2.7, 3.3, -.8],
-]
+]'''
 
-layer1 = LayerDense(4, 5)
-layer2 = LayerDense(5, 2)
+X, y = spiral_data(100, 3)
 
+'''plt.scatter(X[:, 0], X[:, 1], c=y, cmap="brg")
+plt.show()'''
+
+
+layer1 = LayerDense(2, 5)
+activation1 = ActivationReLU()
 layer1.forward(X)
-layer2.forward(layer1.output)
+print(layer1.output)
+activation1.forward(layer1.output)
 
-print(layer2.output)
+print(activation1.output)
