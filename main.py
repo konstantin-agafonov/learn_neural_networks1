c21efc251a9060fab8512abddcf0624771dae600
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2023-05-30 15:42:40 +0300

    created one more layer object

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 from LayerDense import LayerDense
 
 X = [
@@ -8,6 +7,9 @@ X = [
 ]
 
 layer1 = LayerDense(4, 5)
+layer2 = LayerDense(5, 2)
 
 layer1.forward(X)
-print(layer1.output)
+layer2.forward(layer1.output)
+
+print(layer2.output)
